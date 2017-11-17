d2450c09255d25d7fe39f4771c5184d0d11b67f2
derksendaniel <derksendaniel@102WKS02.bsz.edu>
2017-11-17 11:50:41 +0100

    erster commit

---
+++ Kurs.java
@@ -0,0 +1,56 @@
+import java.io.*;
+public class Kurs
+{
+	BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+	double euro,dollar,erg =0;
+	String eingabe=null;
+	
+	public Kurs(double dollar)
+	{
+		this.dollar=dollar;
+		gebeEin();
+	}
+	
+	public static void main(String arg[])
+	{
+		new Kurs(1.48);
+	}
+	
+	private void gebeEin() 
+	{
+
+		do
+		{
+			try
+			{
+				System.out.print("Euro eingeben bitte : ");
+				eingabe=in.readLine();
+				euro=Double.valueOf(eingabe.replace(',','.')).doubleValue();
+				
+				if(euro > 0)
+				{
+					erg =euro*dollar;
+					System.out.println("Dollarwert = "+ erg);
+				}
+			}
+			
+			catch(ArithmeticException e)
+			{
+				System.out.println("Numerischer Wert erforderlich");
+			}
+			catch(IOException e)
+			{
+				System.out.println("Ein/Ausgabefehler");
+			}
+			catch(NumberFormatException e)
+			{
+				System.out.println("Numerischer Wert erforderlich");
+			}
+			catch(Exception e)
+			{
+				System.out.println(e.getMessage() +"Betreuer holen");
+			}
+		}
+		while (euro!=0);
+	}
+}
